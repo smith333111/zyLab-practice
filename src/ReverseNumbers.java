e480b6f87338062e5766796b80d6b3701cc3cc3d
smith333111 <parshin.19973@gmail.com>
2025-08-05 21:40:01 -0400

    Update ReverseNumbers.java

--- src/ReverseNumbers.java
+++ src/ReverseNumbers.java
@@ -5,7 +5,7 @@ public class ReverseNumbers {
         Scanner scanner = new Scanner(System.in);
         int[] userList = new int[20];   // List of numElement integers specified by the user
         int numElements;                // Number of integers in user's list
-        // Add more variables as needed
+
 
         numElements = scanner.nextInt();   // Input begins with number of integers that follow
 
